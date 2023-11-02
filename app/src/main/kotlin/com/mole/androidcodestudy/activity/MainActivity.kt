d0bdd54c6cc202941e1178c1871807068aa6c61e
holdonly <suliliveinchina@gmail.com>
2023-11-02 16:32:58 +0800

    feat viewmodel通过扩展函数绑定

--- app/src/main/kotlin/com/mole/androidcodestudy/activity/MainActivity.kt
+++ app/src/main/kotlin/com/mole/androidcodestudy/activity/MainActivity.kt
@@ -4,25 +4,18 @@ import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.transition.AutoTransition
-import android.transition.Transition
 import android.transition.TransitionManager
-import android.util.TypedValue
 import android.view.View
-import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
-import com.mole.androidcodestudy.di.HiltTestInterface
-import com.mole.androidcodestudy.viewmodel.MainViewModel
-import com.mole.androidcodestudy.R
 import com.mole.androidcodestudy.adapter.PageBean
 import com.mole.androidcodestudy.adapter.PagesAdapter
 import com.mole.androidcodestudy.databinding.ActivityMainBinding
+import com.mole.androidcodestudy.di.HiltTestInterface
 import com.mole.androidcodestudy.extension.viewBinding
+import com.mole.androidcodestudy.extension.viewModelProvider
+import com.mole.androidcodestudy.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.BufferedWriter
 import java.io.IOException
@@ -36,15 +29,15 @@ class MainActivity : BaseActivity() {
 
     @Inject
     lateinit var hiltTestInterface: HiltTestInterface
-    private lateinit var viewModel : MainViewModel
+    private val viewModel: MainViewModel by viewModelProvider()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+//        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setClick()
 
-        viewModel.pet.observe(this){
-            binding.clickButton.text = it?.data?.name?:"null"
+        viewModel.pet.observe(this) {
+            binding.clickButton.text = it?.data?.name ?: "null"
         }
         hiltTestInterface.print()
         autoTransitionTest()
@@ -55,14 +48,14 @@ class MainActivity : BaseActivity() {
 
     }
 
-    private fun softInputMethodTest(){
+    private fun softInputMethodTest() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.toggleSoftInput(0,InputMethodManager.HIDE_NOT_ALWAYS)
+        imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS)
     }
 
 
-    private fun setClick(){
-        binding.clickButton.setOnClickListener{
+    private fun setClick() {
+        binding.clickButton.setOnClickListener {
             viewModel.updatePet()
         }
         binding.fileButton.setOnClickListener {
@@ -73,7 +66,7 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    private fun tryFileOutput(){
+    private fun tryFileOutput() {
         try {
             val output = openFileOutput("data", Context.MODE_APPEND)
             val writer = BufferedWriter(OutputStreamWriter(output))
@@ -83,7 +76,7 @@ class MainActivity : BaseActivity() {
                 }
             }
         } catch (e: IOException) {
-            Toast.makeText(applicationContext,"写入文件异常",Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "写入文件异常", Toast.LENGTH_SHORT).show()
             e.printStackTrace()
         }
     }
@@ -93,30 +86,31 @@ class MainActivity : BaseActivity() {
         hiltTestInterface.print()
     }
 
-    private fun tryGC(){
+    private fun tryGC() {
         System.gc()
         Runtime.getRuntime().gc()
     }
 
-    private fun autoTransitionTest(){
+    private fun autoTransitionTest() {
         val autoTransition = AutoTransition()
         autoTransition.startDelay = 500
         autoTransition.duration = 2000
         autoTransition.interpolator = AccelerateDecelerateInterpolator() // 插值器
 
         binding.hideButton.setOnClickListener {
-            TransitionManager.beginDelayedTransition(binding.clRoot,autoTransition)
+            TransitionManager.beginDelayedTransition(binding.clRoot, autoTransition)
             //rotation,translateX无效
             //直接修改left，设置textSize改变大小，以及设置visibility都有效
             //同时设置时，最后一项有动画效果
             //binding.hideText.left += 20
             //binding.hideText.setTextSize(TypedValue.COMPLEX_UNIT_SP,20f)
-            binding.hideText.visibility = if(binding.hideText.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+            binding.hideText.visibility =
+                if (binding.hideText.visibility == View.VISIBLE) View.GONE else View.VISIBLE
         }
     }
 
-    companion object{
-        val pages:List<PageBean> = mapOf(
+    companion object {
+        val pages: List<PageBean> = mapOf(
             "自定义View" to CustomViewTestActivity::class.java,
             "位置" to LocationActivity::class.java,
             "pickMedia" to PickMediaActivity::class.java,
---
+++ app/src/main/kotlin/com/mole/androidcodestudy/extension/ViewModelDelegate.kt
@@ -0,0 +1,24 @@
+package com.mole.androidcodestudy.extension
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+/**
+ * <pre>
+ *     author : holdonly
+ *     e-mail : suliliveinchina@gmail.com
+ *     time   : 2023/11/02
+ *     desc   : reified实现编译期获取泛型类型，完成viewModel绑定，思路参照ActivityViewBindingDelegate
+ *     version: 1.0
+ * </pre>
+ */
+class ViewModelDelegate<VM:ViewModel>(private val clazz:Class<VM>) : ReadOnlyProperty<AppCompatActivity,VM>{
+    override fun getValue(thisRef: AppCompatActivity, property: KProperty<*>): VM {
+        return ViewModelProvider(thisRef)[clazz]
+    }
+}
+
+inline fun<reified VM:ViewModel> AppCompatActivity.viewModelProvider() = ViewModelDelegate(VM::class.java)
