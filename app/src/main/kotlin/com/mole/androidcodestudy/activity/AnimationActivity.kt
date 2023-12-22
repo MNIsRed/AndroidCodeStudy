af4827dd7b2dbf548a601f4eb807c2fa9ee9c5c2
holdonly <suliliveinchina@gmail.com>
2023-12-22 23:54:27 +0800

    feat 尝试实现掘金头像越转越快的效果

--- app/src/main/kotlin/com/mole/androidcodestudy/activity/AnimationActivity.kt
+++ app/src/main/kotlin/com/mole/androidcodestudy/activity/AnimationActivity.kt
@@ -1,10 +1,12 @@
 package com.mole.androidcodestudy.activity
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.transition.AutoTransition
 import android.transition.TransitionManager
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.Interpolator
 import com.mole.androidcodestudy.databinding.ActivityAnimationBinding
 import com.mole.androidcodestudy.extension.viewBinding
 
@@ -17,12 +19,13 @@ import com.mole.androidcodestudy.extension.viewBinding
  *     version: 1.0
  * </pre>
  */
-class AnimationActivity : BaseActivity(){
+class AnimationActivity : BaseActivity() {
     private val binding by viewBinding(ActivityAnimationBinding::inflate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         autoTransitionTest()
+        infinityScrollImage()
     }
 
     private fun autoTransitionTest() {
@@ -44,4 +47,49 @@ class AnimationActivity : BaseActivity(){
                 if (binding.hideText.visibility == View.VISIBLE) View.GONE else View.VISIBLE
         }
     }
+
+    private var infinityRotationAnimator: ValueAnimator? = null
+    private fun infinityScrollImage() {
+        ValueAnimator.ofFloat(0f, 360f * 200).apply {
+            duration = 200 * 1000
+            repeatCount = 1
+            interpolator = CustomInterpolator()
+            addUpdateListener {
+                val animatedValue = it.animatedValue as Float
+                binding.ivAvatar.rotation = animatedValue
+            }
+        }.also {
+            infinityRotationAnimator = it
+        }
+
+        binding.ivAvatar.setOnClickListener {
+            infinityRotationAnimator?.let {
+                if (it.isRunning) {
+                    it.cancel()
+                    binding.ivAvatar.rotation = 0f
+                } else {
+                    it.start()
+                }
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        infinityRotationAnimator?.let {
+            if (it.isRunning) {
+                it.cancel()
+            }
+        }
+    }
+
+    // 自定义插值器
+    class CustomInterpolator : Interpolator {
+        override fun getInterpolation(input: Float): Float {
+            // 应用指数上升的插值器
+            //实践证明，插值器的值可以比1大，借此可以实现越转越快的效果
+            return (input * 200) * input * 10 / 200
+        }
+    }
+
 }
