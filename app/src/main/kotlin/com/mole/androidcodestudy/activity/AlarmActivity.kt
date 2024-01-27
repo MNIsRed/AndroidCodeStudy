f9d1f710f62a1044dd40fd08a8fd52a13f01f321
holdonly <suliliveinchina@gmail.com>
2024-01-27 13:18:54 +0800

    测试定时任务发通知

--- app/src/main/kotlin/com/mole/androidcodestudy/activity/AlarmActivity.kt
+++ app/src/main/kotlin/com/mole/androidcodestudy/activity/AlarmActivity.kt
@@ -1,16 +1,34 @@
 package com.mole.androidcodestudy.activity
 
+import android.app.Activity
 import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import androidx.core.app.AlarmManagerCompat
+import androidx.core.app.NotificationCompat
+import com.carterchen247.alarmscheduler.AlarmScheduler
+import com.carterchen247.alarmscheduler.logger.AlarmSchedulerLogger
+import com.carterchen247.alarmscheduler.model.AlarmConfig
+import com.carterchen247.alarmscheduler.model.DataPayload
+import com.carterchen247.alarmscheduler.model.ScheduleResult
+import com.carterchen247.alarmscheduler.task.AlarmTask
+import com.carterchen247.alarmscheduler.task.AlarmTaskFactory
+import com.mole.androidcodestudy.CustomApplication
+import com.mole.androidcodestudy.R
 import com.mole.androidcodestudy.databinding.ActivityAlarmBinding
 import com.mole.androidcodestudy.extension.viewBinding
 import com.mole.androidcodestudy.receiver.ALARM_ACTION
 import com.mole.androidcodestudy.receiver.AlarmBroadcastReceiver
 import java.time.Duration
+import java.util.Date
 
 /**
  * <pre>
@@ -21,7 +39,7 @@ import java.time.Duration
  *     version: 1.0
  *     参考：
  *     https://developer.android.com/about/versions/14/changes/schedule-exact-alarms?hl=zh-cn
- *     https://developer.android.com/about/versions/14/changes/schedule-exact-alarms?hl=zh-cn
+ *     https://carterchen247.medium.com/android-14-behavior-change-schedule-exact-alarms-are-denied-by-default-7563a814dee4
  * </pre>
  */
 class AlarmActivity : BaseActivity() {
@@ -30,6 +48,7 @@ class AlarmActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.btAdd.setOnClickListener {
+//            getAlarmScheduler()
             addAlarm()
         }
     }
@@ -38,6 +57,7 @@ class AlarmActivity : BaseActivity() {
         val receiverIntent = Intent().apply {
             setClass(this@AlarmActivity, AlarmBroadcastReceiver::class.java)
             action = ALARM_ACTION
+            setComponent(ComponentName(this@AlarmActivity.packageName,"${this@AlarmActivity.packageName}.receiver.AlarmBroadcastReceiver"))
         }
         (getSystemService(Context.ALARM_SERVICE) as? AlarmManager)?.let { alarmManager ->
             val duration = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -45,9 +65,114 @@ class AlarmActivity : BaseActivity() {
             } else {
                 60 * 1000
             }
-            PendingIntent.getBroadcast(this, 0, receiverIntent, PendingIntent.FLAG_IMMUTABLE).also {
-                alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5 * 1000, it)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                if (!alarmManager.canScheduleExactAlarms()) {
+                    openExactAlarmSettingPage()
+                    return
+                }
             }
+            PendingIntent.getBroadcast(this, 0, receiverIntent, PendingIntent.FLAG_IMMUTABLE).also {
+//                AlarmManagerCompat.setAlarmClock(
+//                    alarmManager,
+//                    System.currentTimeMillis() + 5 * 1000,
+//                    it,
+//                    it
+//                )
+//                alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5 * 1000, it)
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5 * 1000, it)
+
+            }
+        }
+    }
+
+    private fun getAlarmScheduler() {
+        AlarmScheduler.setLogger(AlarmSchedulerLogger.DEBUG)
+
+        AlarmScheduler.setAlarmTaskFactory(object : AlarmTaskFactory {
+            override fun createAlarmTask(alarmType: Int): AlarmTask {
+                return MyAlarmTask()
+            }
+        })
+
+        val config = AlarmConfig(
+            Date().time + 10000L, // trigger time
+            MyAlarmTask.TYPE
+        ) {
+            dataPayload("reminder" to "have a meeting")
+        }
+
+        AlarmScheduler.schedule(config) { result ->
+            when (result) {
+                is ScheduleResult.Success -> {
+                    // alarm scheduling success!
+                }
+
+                is ScheduleResult.Failure -> {
+                    when (result) {
+                        ScheduleResult.Failure.CannotScheduleExactAlarm -> {
+                            // handle scenarios like user disables exact alarm permission
+                            openExactAlarmSettingPage()
+                        }
+
+                        is ScheduleResult.Failure.Error -> {
+                            // handle error
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    fun Activity.openExactAlarmSettingPage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            startActivity(Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM).apply {
+                //跳转app单独设置权限，否则会进入一个app列表
+                data = Uri.parse("package:"+this@AlarmActivity.packageName)
+            })
+        }
+    }
+
+    class MyAlarmTask : AlarmTask {
+        override fun onAlarmFires(alarmId: Int, dataPayload: DataPayload) {
+            // do something with dataPayload you set
+            (CustomApplication.getInstance()
+                .getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager)?.let { notificationManager ->
+                NotificationCompat.Builder(CustomApplication.getInstance()).apply {
+                    setContentTitle("测试通知标题")
+                    setContentText("测试通知内容")
+                    //默认优先级
+                    setPriority(NotificationCompat.PRIORITY_MAX)
+                    //提醒方式 声音，闪光等
+                    setDefaults(NotificationCompat.DEFAULT_ALL)
+                    setSmallIcon(R.mipmap.ic_launcher)
+                    setAutoCancel(true)
+                    //必须设置ChannelId
+                    setChannelId("1")
+                    PendingIntent.getActivity(
+                        CustomApplication.getInstance(),
+                        0,
+                        Intent(CustomApplication.getInstance(), MainActivity::class.java),
+                        PendingIntent.FLAG_IMMUTABLE
+                    ).also {
+                        setContentIntent(it)
+                    }
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        // 8.0以上系统，通知需要加channel信息
+                        val channel = NotificationChannel(
+                            "1", "channel",
+                            NotificationManager.IMPORTANCE_HIGH
+                        )
+                        notificationManager.createNotificationChannel(channel)
+                    }
+                    //Android13 需要权限POST_NOTIFICATIONS
+                    notificationManager.notify(0, build())
+                }
+            }
+        }
+
+        companion object {
+            const val TYPE = 1
         }
     }
 }
--- app/src/main/kotlin/com/mole/androidcodestudy/receiver/AlarmBroadcastReceiver.kt
+++ app/src/main/kotlin/com/mole/androidcodestudy/receiver/AlarmBroadcastReceiver.kt
@@ -1,16 +1,18 @@
 package com.mole.androidcodestudy.receiver
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationCompat.DEFAULT_ALL
 import androidx.core.app.NotificationCompat.PRIORITY_DEFAULT
 import com.mole.androidcodestudy.R
 import com.mole.androidcodestudy.activity.MainActivity
-import okhttp3.internal.notify
 
 
 /**
@@ -23,10 +25,12 @@ import okhttp3.internal.notify
  * </pre>
  */
 const val ALARM_ACTION = "com.mole.androidcodestudy.alarm_action"
+
 class AlarmBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        context?.let {context->
-            (context.getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager)?.let {notificationManager->
+        Log.d("AlarmBroadcastReceiver","AlarmBroadcastReceiver触发")
+        context?.let { context ->
+            (context.getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager)?.let { notificationManager ->
                 NotificationCompat.Builder(context).apply {
                     setContentTitle("测试通知标题")
                     setContentText("测试通知内容")
@@ -36,12 +40,25 @@ class AlarmBroadcastReceiver : BroadcastReceiver() {
                     setDefaults(DEFAULT_ALL)
                     setSmallIcon(R.mipmap.ic_launcher)
                     setAutoCancel(true)
-
-                    PendingIntent.getActivity(context,0,Intent(context,MainActivity::class.java),PendingIntent.FLAG_IMMUTABLE).also {
+                    setChannelId("2333")
+                    PendingIntent.getActivity(
+                        context,
+                        0,
+                        Intent(context, MainActivity::class.java),
+                        PendingIntent.FLAG_IMMUTABLE
+                    ).also {
                         setContentIntent(it)
                     }
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        // 8.0以上系统，通知需要加channel信息
+                        val channel = NotificationChannel(
+                            "2333", "channel",
+                            NotificationManager.IMPORTANCE_HIGH
+                        )
+                        notificationManager.createNotificationChannel(channel)
+                    }
                     //Android13 需要权限POST_NOTIFICATIONS
-                    notificationManager.notify(0,build())
+                    notificationManager.notify(0, build())
                 }
             }
         }
