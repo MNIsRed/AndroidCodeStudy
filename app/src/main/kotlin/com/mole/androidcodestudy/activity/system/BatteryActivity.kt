239d6f1dce1de6be5966b27b249f9ae9f7cb2550
holdonly <suliliveinchina@gmail.com>
2025-03-11 15:19:29 +0800

    test:安卓电池&ConstraintLayout

--- app/src/main/kotlin/com/mole/androidcodestudy/activity/system/BatteryActivity.kt
+++ app/src/main/kotlin/com/mole/androidcodestudy/activity/system/BatteryActivity.kt
@@ -1,5 +1,6 @@
 package com.mole.androidcodestudy.activity.system
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.ComponentName
 import android.content.Context
@@ -10,9 +11,7 @@ import android.os.BatteryManager
 import android.os.Build
 import android.os.Bundle
 import android.os.PowerManager
-import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.IntentCompat
 import com.mole.androidcodestudy.R
 import com.mole.androidcodestudy.databinding.ActivityBatteryBinding
 import com.mole.androidcodestudy.extension.viewBinding
@@ -20,20 +19,23 @@ import com.mole.androidcodestudy.extension.viewBinding
 class BatteryActivity : AppCompatActivity() {
     private val binding: ActivityBatteryBinding by viewBinding()
     private lateinit var batteryReceiver: BroadcastReceiver
+    private var hasIgnored = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_battery)
-        var hasIgnored = false
-        (getSystemService(POWER_SERVICE) as? PowerManager)?.let { power ->
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                hasIgnored = power.isIgnoringBatteryOptimizations(packageName)
-            }
-        }
+
 
         batteryReceiver = object : BroadcastReceiver() {
+            @SuppressLint("SetTextI18n")
             override fun onReceive(context: Context?, intent: Intent?) {
                 if (intent?.action == Intent.ACTION_BATTERY_CHANGED) {
+                    (getSystemService(POWER_SERVICE) as? PowerManager)?.let { power ->
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            hasIgnored = power.isIgnoringBatteryOptimizations(packageName)
+                        }
+                    }
+
                     val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
                     val scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
                     val batteryPct = level * 100 / scale.toFloat()
@@ -51,27 +53,50 @@ class BatteryActivity : AppCompatActivity() {
             }
         }
 
-
         binding.btnBatterySettings.setOnClickListener {
-            val intent = Intent()
-            //需要Manifest 声明REQUEST_IGNORE_BATTERY_OPTIMIZATIONS权限
-            //intent.action = android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
-            //intent.setData(Uri.parse("package:${packageName}"))
-
-            //拉起智能管理页面
-            //intent.action = android.provider.Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS
-
-            //和 2 一样
-            //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            //
-            //intent.addCategory(Intent.CATEGORY_LAUNCHER)
-            //
-            //val cn = ComponentName.unflattenFromString("com.android.settings/.Settings\$HighPowerApplicationsActivity")
-            //
-            //intent.setComponent(cn)
-
-            startActivity(intent)
+            showBatteryIgnoreDialog()
         }
+    }
+
+    /**
+     * 请求忽略电池限制弹窗，但是哪怕手动打开了，似乎会自动关闭这项设置
+     */
+    private fun showBatteryIgnoreDialog() {
+        val intent = Intent()
+        //需要Manifest 声明REQUEST_IGNORE_BATTERY_OPTIMIZATIONS权限
+        intent.action = android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
+        intent.setData(Uri.parse("package:${packageName}"))
+        startActivity(intent)
+    }
+
+    /**
+     * 打开智能管理页面
+     */
+    private fun openBatteryManager1() {
+        val intent = Intent()
+        intent.action = android.provider.Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS
+        startActivity(intent)
+    }
+
+    /**
+     * 打开智能管理页面
+     */
+    private fun openBatteryManager2() {
+        val intent = Intent()
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+        val cn =
+            ComponentName.unflattenFromString("com.android.settings/.Settings\$HighPowerApplicationsActivity")
+        intent.setComponent(cn)
+        startActivity(intent)
+    }
+
+    /**
+     * 打开电池页面
+     */
+    private fun openBatteryUsage() {
+        val intent = Intent(Intent.ACTION_POWER_USAGE_SUMMARY)
+        startActivity(intent)
     }
 
     override fun onResume() {
